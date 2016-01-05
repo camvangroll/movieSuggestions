a8f26b193fa1ffa21c340c6bb93ba7f5f3871432
Patrick Cameron <patrickcameron@Patricks-Mac-Book-Pro.local>
2016-01-05 09:47:37 -0500

    All AJAX calls now working. Added gitignore file.

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@ var movieDBKey = "68648bc22dd03754bbe2b368551f7eb2";
 
 var movieApp = {};
 
-var imdbNum = 2593224;
-
 var movieInfo = {};
 
 
@@ -15,50 +13,55 @@ movieApp.getMovies = function() {
 		method: 'GET',
 		dataType: 'jsonp'
 	}).then(function(data) {
-		console.log(data);
 		movieInfo = data;
-		for (var i in data.results) {
-			$('.results').append('<div class="movie">' + '<img class="movie-poster" src="http://image.tmdb.org/t/p/w500' + data.results[i].poster_path + '">' + '<p>' + data.results[i].title + '</p>' + '<p class="description">' + data.results[i].overview + '</p>' + '<p>' + data.results[i].id + '</p>' + '</div>');
-		};
+		console.log(movieInfo);
+		movieApp.getIMDBNumber();
 	});
 };
 
-movieApp.getIMDBNumber = function(movieNum) {
-	$.ajax({
-		url: 'http://api.themoviedb.org/3/movie/' + movieNum + '?api_key=' + movieDBKey,
-		method: 'GET',
-		dataType: 'jsonp'
-	}).then(function(data) {
-		console.log('imdb number is ' + data.imdb_id);
-	})
-};
-
-movieApp.searchBechdel = function() {
-	movieApp.searchBechdel = function() {
-	$.ajax({
-		url: 'http://proxy.hackeryou.com',
-		method: 'GET',
-		dataType: 'json',
-		data: {
-			reqUrl: 'http://bechdeltest.com/api/v1/getMovieByImdbId?' + imdbNum
-		}
-	}).then(function(data) {
-		console.log(data);
-	});
+movieApp.getIMDBNumber = function() {
+	for (var i in movieInfo.results) {
+		$.ajax({
+			url: 'http://api.themoviedb.org/3/movie/' + movieInfo.results[i].id + '?api_key=' + movieDBKey,
+			method: 'GET',
+			dataType: 'jsonp'
+		}).then(function(data) {
+			var imdbNum = data.imdb_id;
+			imdbNum = imdbNum.substr(2);
+			imdbNum = imdbNum.toString();
+			console.log(imdbNum);
+			$.ajax({
+				url: 'http://proxy.hackeryou.com',
+				method: 'GET',
+				dataType: 'json',
+				data: {
+					reqUrl: 'http://bechdeltest.com/api/v1/getMovieByImdbId',
+					imdbid: imdbNum
+				}
+				}).then(function(data) {
+					console.log(data.rating);
+					console.log(movieInfo.results[i]);
+					movieInfo.results[i].bechdelRating = data.rating;
+					console.log(movieInfo.results[i].bechdelRating);
+			});
+		});
 	};
+	movieApp.displayResults();
 };
 
 movieApp.results = function() {
 	$('form').on('submit', function(e) {
 		e.preventDefault();
 		movieApp.getMovies();
-		// movieApp.getIMDBNumber(861);
-		// movieApp.searchBechdel();
-		console.log("results");
-	});
-	
-}
+	});	
+};
 
+movieApp.displayResults = function() {
+	$('.results').html('');
+	for (var i in movieInfo.results) {
+		$('.results').append('<div class="movie">' + '<img class="movie-poster" src="http://image.tmdb.org/t/p/w500' + movieInfo.results[i].poster_path + '">' + '<p>' + movieInfo.results[i].title + '</p>' + '<p class="description">' + movieInfo.results[i].overview + '</p>' + '<p>' + movieInfo.results[i].id + '</p>' + '<p>' + movieInfo.bechdelRating + '</p>' + '</div>');
+	};
+};
 
 $(function() {
 	movieApp.results();
